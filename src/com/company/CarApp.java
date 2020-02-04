dac0df26a29498806365bf9dc6369eeafaeb2dbf
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-04 15:49:19 -0500

    final code

--- src/com/company/CarApp.java
+++ src/com/company/CarApp.java
@@ -1,15 +1,38 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CarApp {
 
     public static void main(String[] args) {
-	Car car = new Car();
-	car.setModel("Porche");
-	car.setMake("red");
-	car.setSpeed(80);
-        System.out.println(car.start());
-        System.out.println(car.accelerate());
-	    car.getSpeed();
-        System.out.println(car.stop());
+        ArrayList<Car> cars= new ArrayList<>();
+        Car car = new Car();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter model ");
+        String make=sc.next();
+        car.setMake(make);
+
+        System.out.println("Enter year");
+        int year=sc.nextInt();
+        car.setYear(year);
+        System.out.println("Enter make");
+        String model=sc.next();
+        car.setModel(model);
+        System.out.println("Enter speed");
+        int speed=sc.nextInt();
+        car.setSpeed(speed);
+
+
+//        car.setModel("Porche");
+//        car.setMake("red");
+//        car.setSpeed(80);
+        cars.add(car);
+        for(Car c : cars) {
+            System.out.println(car.start());
+            System.out.println(car.accelerate());
+            car.getSpeed();
+            System.out.println(car.stop());
+        }
     }
 }
